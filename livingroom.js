0adfefd654acda7a88349652a45e3b8960608ad2
MysticBlue1102 <101375434+MysticBlue1102@users.noreply.github.com>
2023-01-25 19:31:06 -0700

    Add files via upload

---
+++ livingroom.js
@@ -0,0 +1,32 @@
+status = "";
+img = "";
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocssd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function preload()
+{
+    img = loadImage('livingroom.jpg');
+}
+
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
